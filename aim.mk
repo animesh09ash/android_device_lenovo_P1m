178d3c7f1b2e914fab6f3523e13904a3e5e751bd
z3r0c00k_2k <mjack1327@gmail.com>
2017-12-01 16:29:00 +0000

    Aim-fy

---
+++ aim.mk
@@ -0,0 +1,27 @@
+## Specify phone tech before including full_phone
+
+# Release name
+PRODUCT_RELEASE_NAME := P1m
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/aim/config/common_full_phone.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.aim.maintainer=z3r0c00l_2k 
+# Inherit device configuration
+$(call inherit-product, device/lenovo/P1m/device_P1m.mk)
+
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := P1m
+PRODUCT_NAME := aim_P1m
+PRODUCT_BRAND := lenovo
+PRODUCT_MODEL := Lenovo P1m
+PRODUCT_MANUFACTURER := lenovo
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+# Available languages
+PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG th_TH
